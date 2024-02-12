59fded8492c955abd33906797364275453333291
Louis Colson <ljcolson27@gmail.com>
2024-02-12 16:41:54 -0500

    Update transformer.py

--- se3_transformer/model/transformer.py
+++ se3_transformer/model/transformer.py
@@ -29,13 +29,13 @@ import torch.nn as nn
 from dgl import DGLGraph
 from torch import Tensor
 
-from se3_transformer.model.model.basis import get_basis, update_basis_with_fused
-from se3_transformer.model.model.layers.attention import AttentionBlockSE3
-from se3_transformer.model.model.layers.convolution import ConvSE3, ConvSE3FuseLevel
-from se3_transformer.model.model.layers.norm import NormSE3
-from se3_transformer.model.model.layers.pooling import GPooling
+from se3_transformer.model.basis import get_basis, update_basis_with_fused
+from se3_transformer.model.layers.attention import AttentionBlockSE3
+from se3_transformer.model.layers.convolution import ConvSE3, ConvSE3FuseLevel
+from se3_transformer.model.layers.norm import NormSE3
+from se3_transformer.model.layers.pooling import GPooling
 from se3_transformer.model.runtime.utils import str2bool
-from se3_transformer.model.model.fiber import Fiber
+from se3_transformer.model.fiber import Fiber
 
 
 class Sequential(nn.Sequential):
